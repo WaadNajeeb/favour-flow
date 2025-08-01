dd4eec4f83738e3a8f00271054b924b54726ec22
WaadNajeeb <waad.najeeb@education.nsw.gov.au>
2025-08-01 20:58:03 +1000

    init project frontend and backend

---
+++ client/src/app/AppRoutes.ts
@@ -0,0 +1,6 @@
+import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot, Routes } from '@angular/router';
+import { inject } from '@angular/core';
+
+export const ROUTES: Routes = [
+
+];
---
+++ client/src/main.ts
@@ -0,0 +1,31 @@
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { withInterceptorsFromDi, provideHttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { ROUTES } from './app/AppRoutes';
+import { AuthInterceptor } from './app/interceptors/auth.interceptor';
+import { provideToastr } from 'ngx-toastr';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(),
+    importProvidersFrom(BrowserModule),
+    provideRouter(ROUTES),
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    provideAnimations(),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideToastr({
+      closeButton: true,
+      positionClass: 'toast-bottom-center'
+    })
+
+  ]
+})
+  .catch(err => console.error(err));
