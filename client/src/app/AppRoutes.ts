e964643e097d92b3c32680960c59471112b59213
WaadNajeeb <waad.najeeb@education.nsw.gov.au>
2025-08-03 16:17:04 +1000

    sign in

--- client/src/app/AppRoutes.ts
+++ client/src/app/AppRoutes.ts
@@ -7,15 +7,23 @@ import { UserFavoursComponent } from "./user-favours/user-favours.component";
 
 
 export const ROUTES: Routes = [
-{
+  {
     path: '',
-    component: MainComponent,
+    loadComponent: () => import('./main/main.component').then(m => m.MainComponent),
     children: [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
-      { path: '', component: HomeComponent },
-      { path: 'leaderboard', component: LeaderboardComponent },
-      { path: 'my-favours', component: UserFavoursComponent }
+      { path: '', loadComponent: () => import('./home/home.component').then(m => m.HomeComponent) },
+      { path: 'leaderboard', loadComponent: () => import('./leaderboard/leaderboard.component').then(m => m.LeaderboardComponent) },
+      { path: 'my-favours', loadComponent: () => import('./user-favours/user-favours.component').then(m => m.UserFavoursComponent) },
     ]
+  },
+  {
+    path: 'sign-in',
+    loadComponent: () => import('./auth/sign-in/sign-in.component').then(m => m.SignInComponent)
+  },
+  {
+    path: 'sign-up',
+    loadComponent: () => import('./auth/sign-up/sign-up.component').then(m => m.SignUpComponent)
   },
   { path: '**', redirectTo: 'home' } // fallback
 ];
---
+++ client/src/app/auth/sign-in/sign-in.component.ts
@@ -0,0 +1,30 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatError, MatFormField, MatFormFieldModule } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { Form, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+@Component({
+  selector: 'app-sign-in',
+  imports: [MatIcon, MatFormFieldModule, MatCheckboxModule, MatButtonModule, ReactiveFormsModule, MatError, CommonModule, MatInputModule],
+  templateUrl: './sign-in.component.html',
+  styleUrl: './sign-in.component.scss'
+})
+export class SignInComponent {
+
+   private fb = inject(FormBuilder);
+   signInForm = this.fb.group<SignInForm>({
+    email: new FormControl<string | null>(null, [Validators.required, Validators.email]),
+    password: new FormControl<string | null>(null, [Validators.required]),
+    rememberMe: new FormControl<boolean | null>(false)
+  });
+}
+
+
+export interface SignInForm {
+  email: FormControl<string | null>;
+  password:FormControl<string | null>;
+  rememberMe: FormControl<boolean | null>;
+}
